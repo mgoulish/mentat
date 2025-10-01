58d1d1b9a9a7c597d1dafb805462b50393ac06f9
mick <mgoulish@redhat.com>
2025-10-01 02:50:10 -0400

    events in grand list at top level

--- mentat.py
+++ mentat.py
@@ -26,10 +26,12 @@ def string_to_microseconds_since_epoch ( s ):
 
 def new_mentat ( root ) :
   keys = [ 'root',
-           'sites' ]
+           'sites',
+           'events' ]
   mentat = dict.fromkeys ( keys, None )
-  mentat['root']  = root
-  mentat['sites'] = []
+  mentat['root']   = root
+  mentat['sites']  = []
+  mentat['events'] = []
   print ( f"mentat info: new mentat created with root {root}" )
   return mentat
 
@@ -80,75 +82,92 @@ def new_line ( log_line, file_path, line_number, timestamp, router, site ) :
 
 
 
-def read_router_log ( router, log_file_path, line_list ) :
-  print ( f"mentat info: read_router_log: router: {router['name']} file: {log_file_path}" )
+def read_router_log ( mentat, router, log_file_path, line_list, router_name_prefix ) :
+  if router_name_prefix :
+    router_name = router_name_prefix + ' ' + router['name']
+  else :
+    router_name = router['name']
+
+  print ( f"mentat info: read_router_log: router: {router_name} file: {log_file_path}" )
   timestamp_regex = r'^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})'
-  line_count = 1
+  line_count = 0
   with open(log_file_path) as f:
     content = f.readlines()
     for line_str in content :
       line_str = line_str.rstrip()
       match = re.match ( timestamp_regex, line_str )
       if match :
-        line = new_line ( line_str, log_file_path, line_count, match.group(1), router['name'], router['site'] )
+        line_count += 1
+        line = new_line ( line_str, log_file_path, line_count, match.group(1), router_name, router['site'] )
         line_list.append ( line )
-      line_count += 1
+        # Also append this line to the grand top-level list
+        mentat['events'].append ( line )
   print ( f"mentat info: read_router_log: read {line_count} lines" )
 
 
 
+def print_events ( mentat ) :
+  print ( "\n\n\n" )
+  for site in mentat['sites'] :
+    print ( "========================================================" )
+    print (   site['name']  )
+    print ( "========================================================" )
+    for router in site['routers'] :
+      print ( f"  router:  {router['name']}" )
+  
+      print ( "\n\nprevious events: " )
+      for event in router['previous_events'] :
+        pprint.pprint ( event )
+  
+      print ( "\n\ncurrent events: " )
+      for event in router['current_events'] :
+        pprint.pprint ( event )
+  
+
+  
+def read_events ( mentat ) :
+  site_names = get_dirs(mentat['root'])
+  for site_name in site_names :
+    site = new_site ( site_name )
+    mentat['sites'].append(site)
+    pods_path = f"{root}/{site_name}/pods"
+    pod_names = get_dirs(pods_path)
+    for pod_name in pod_names :
+      if pod_name.startswith('skupper-router') :
+        router = new_router ( pod_name, site_name )
+        site['routers'].append ( router )
+        logs_path = f"{pods_path}/{pod_name}/logs"
+        file_names = os.listdir(logs_path)
+        for basename in file_names :
+          if basename.startswith ( 'router-logs' ) :
+            file_name = f"{logs_path}/{basename}"
+            # There may be two files that both start with 
+            # this prefix:
+            #   router-logs-previous.txt, and
+            #   router-logs.txt
+            # We want both.
+            # It doesn't matter in which order we read them,
+            # since the events will all eventually be sorted
+            # into chronological order.
+            if basename == 'router-logs-previous.txt' :
+              print ( f"mentat info: main: reading previous events for router {router['name']}" )
+              read_router_log ( mentat, router, file_name, router['previous_events'], 'previous' )  
+            elif basename == 'router-logs.txt' :
+              print ( f"mentat info: main: reading latest events for router {router['name']}" )
+              read_router_log ( mentat, router, file_name, router['current_events'], None )  
+
+  
+
+    
 #================================================================
 #  Main
 #================================================================
 root   =  sys.argv[1]
 mentat = new_mentat ( root )
 
-site_names = get_dirs(root)
+read_events ( mentat )
 
-for site_name in site_names :
-  site = new_site ( site_name )
-  mentat['sites'].append(site)
-  pods_path = f"{root}/{site_name}/pods"
-  pod_names = get_dirs(pods_path)
-  for pod_name in pod_names :
-    if pod_name.startswith('skupper-router') :
-      router = new_router ( pod_name, site_name )
-      site['routers'].append ( router )
-      logs_path = f"{pods_path}/{pod_name}/logs"
-      file_names = os.listdir(logs_path)
-      for basename in file_names :
-        if basename.startswith ( 'router-logs' ) :
-          file_name = f"{logs_path}/{basename}"
-          # There may be two files that both start with 
-          # this prefix:
-          #   router-logs-previous.txt, and
-          #   router-logs.txt
-          # We want both.
-          # It doesn't matter in which order we read them,
-          # since the events will all eventually be sorted
-          # into chronological order.
-          if basename == 'router-logs-previous.txt' :
-            print ( f"mentat info: main: reading previous events for router {router['name']}" )
-            read_router_log ( router, file_name, router['previous_events'] )  
-          elif basename == 'router-logs.txt' :
-            print ( f"mentat info: main: reading latest events for router {router['name']}" )
-            read_router_log ( router, file_name, router['current_events'] )  
+#print_events ( mentat )
 
-
-
-print ( "\n\n\n" )
-for site in mentat['sites'] :
-  print ( "========================================================" )
-  print (   site['name']  )
-  print ( "========================================================" )
-  for router in site['routers'] :
-    print ( f"  router:  {router['name']}" )
-
-    print ( "\n\nprevious events: " )
-    for event in router['previous_events'] :
-      pprint.pprint ( event )
-
-    print ( "\n\ncurrent events: " )
-    for event in router['current_events'] :
-      pprint.pprint ( event )
+print ( f"mentat now has {len(mentat['events'])} total events" )
 
