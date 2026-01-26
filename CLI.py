7ff1b4cd86de4d9b995b394f54717f3f65a8b01b
mick <mgoulish@redhat.com>
2026-01-25 23:37:51 -0500

    Alphabetize commands in CLI, add wait command, add first script

--- CLI.py
+++ CLI.py
@@ -6,11 +6,14 @@ import pprint
 import copy
 import cmd
 from typing import Dict, Any
+from pathlib import Path
+
 
 
 import debug
 import filters
 import new
+
 
 
 class MentatCLI(cmd.Cmd):
@@ -22,27 +25,6 @@ class MentatCLI(cmd.Cmd):
     self.mentat['cli'] = self
 
 
-  def do_q ( self, arg ) :
-    self.do_quit ( arg )
-
-
-  def do_quit ( self, arg ) :
-    """exit the program"""
-    mentat = self.mentat
-    print ( "quitting..." )
-    sys.exit ( 0 )
-
-  # This just shows the user the start and stop times 
-  # for the current data set.
-  def do_time_range ( self, arg ) :
-    '''Show start amd stop times for entire data set'''
-    mentat = self.mentat
-    n_events        = len(mentat['events'])
-    first_timestamp = mentat['events'][0]['timestamp']
-    last_timestamp  = mentat['events'][n_events - 1]['timestamp']
-    print ( f"  {first_timestamp} to {last_timestamp}" )
-
-  
 
   def do_count ( self, arg ) :
     '''Count events in current filter chain'''
@@ -51,101 +33,17 @@ class MentatCLI(cmd.Cmd):
 
 
 
-  def do_replace ( self, arg ) :
-    '''Replace a filter in your current filter chain'''
+  def do_grep ( self, arg ) :
+    '''Filter log events for the given keyword'''
+    filters.grep ( self.mentat, arg )
+
+
+
+  def do_list ( self, arg ) :    
+    '''List the filtered results of your current filter chain'''
     mentat = self.mentat
     command_line = arg
-    if len(filters.current_filter_chain['filters']) == 0 :
-      print ( "\nThere are no filters in the current filter chain." )
-      return
-  
-    print ( f"command line: |{command_line}|" )
-    words = command_line.split()
-    if len(words) < 2 :
-      print ( "\nGive me an integer argument" )
-      return
-    s = words[1]
-    n = 0
-    try:
-      int(s)
-      n = int(s)
-    except ValueError:
-      print ( f"    {s} is not an integer" )
-      return
-    
-    if n > len(filters.current_filter_chain['filters']) - 1 :
-      print ( f"There are {len(filters.current_filter_chain['filters'])} filters in the current filter chain." )
-      return
-    
-    filters.replacing_filter = n
-    print ( f"Replacing filter {n} please enter a filter command" )
-    print ( f"replace_command: it will replace filter {n}." )
-
-
-
-  def do_show_filter ( self, arg ) :
-    '''Show your current filter chain'''
-    mentat = self.mentat
-    if len(filters.current_filter_chain['filters']) == 0 :
-      print ( '\nThere is no current filter chain.\n' )
-      return
-  
-    print ( f"\nCurrent Filter Chain", end='' )
-    if filters.current_filter_chain['name'] == None :
-      print ( ': (no name yet)' )
-    else :
-      print ( f": {filters.current_filter_chain['name']}" )
-    
-    count = 0
-    for f in filters.current_filter_chain['filters'] :
-      print ( f"Filter {count}: {f['name']}", end='' )
-      for arg in f['args'] :
-        print ( f" {arg} ", end='' )
-      print (' ')
-      count += 1
-    print ( ' ' )
-
-
-
-  def do_save ( self, arg ) :
-    '''Name and save the current filter chain'''
-    mentat = self.mentat
-    command_line = arg
-    if len(filters.current_filter_chain['filters']) == 0 :
-      print ( "\nThere is no current filter chain to save." )
-      return
-    words = command_line.split()
-    if len(words) != 2 :
-      print ( "\n    Usage: save NAME" )
-      return
-    filter_chain_name = words[1]
-    print ( f"filter_chain_name: {filter_chain_name}" )
-    filters.current_filter_chain['name'] = filter_chain_name
-    filters.filter_chains.append(copy.deepcopy(filters.current_filter_chain))
-    filters.current_filter_chain = new.new_filter_chain()
-  
-    print ( "\nSaved Filter Chains:" )
-    for saved_filter in filters.filter_chains :
-      print ( f"    {saved_filter['name']}" )
-    print ( " " )
-  
-  
-  
-  def do_restore ( self, arg ) :
-    '''Restore a saved filter chain to be current'''
-    mentat = self.mentat
-    command_line = arg
-    words = command_line.split()
-    if len(words) != 2 :
-      print ( "\n    Usage: restore NAME" )
-      return
-    filter_chain_name = words[1]
-    for fc in filters.filter_chains : 
-      if filter_chain_name == fc['name'] :
-        filters.current_filter_chain = copy.deepcopy(fc)
-        print ( f"\nrestored filter chain '{fc['name']}'" )
-        return
-    print ( f"can't find filter chain '{fc['name']}'" )
+    filters.list_filtered_results ( mentat )
 
 
 
@@ -187,7 +85,160 @@ class MentatCLI(cmd.Cmd):
     print ( " " )
   
   
+
+  def do_q ( self, arg ) :
+    self.do_quit ( arg )
+
+
+
+  def do_quit ( self, arg ) :
+    """exit the program"""
+    mentat = self.mentat
+    print ( "quitting..." )
+    sys.exit ( 0 )
+
   
+
+  def do_replace ( self, arg ) :
+    '''Replace a filter in your current filter chain'''
+    mentat = self.mentat
+    command_line = arg
+    if len(filters.current_filter_chain['filters']) == 0 :
+      print ( "\nThere are no filters in the current filter chain." )
+      return
+  
+    print ( f"command line: |{command_line}|" )
+    words = command_line.split()
+    if len(words) < 2 :
+      print ( "\nGive me an integer argument" )
+      return
+    s = words[1]
+    n = 0
+    try:
+      int(s)
+      n = int(s)
+    except ValueError:
+      print ( f"    {s} is not an integer" )
+      return
+    
+    if n > len(filters.current_filter_chain['filters']) - 1 :
+      print ( f"There are {len(filters.current_filter_chain['filters'])} filters in the current filter chain." )
+      return
+    
+    filters.replacing_filter = n
+    print ( f"Replacing filter {n} please enter a filter command" )
+    print ( f"replace_command: it will replace filter {n}." )
+
+
+
+  def do_restore ( self, arg ) :
+    '''Restore a saved filter chain to be current'''
+    mentat = self.mentat
+    command_line = arg
+    words = command_line.split()
+    if len(words) != 2 :
+      print ( "\n    Usage: restore NAME" )
+      return
+    filter_chain_name = words[1]
+    for fc in filters.filter_chains : 
+      if filter_chain_name == fc['name'] :
+        filters.current_filter_chain = copy.deepcopy(fc)
+        print ( f"\nrestored filter chain '{fc['name']}'" )
+        return
+    print ( f"can't find filter chain '{fc['name']}'" )
+
+
+
+  def do_save ( self, arg ) :
+    '''Name and save the current filter chain'''
+    mentat = self.mentat
+    command_line = arg
+    if len(filters.current_filter_chain['filters']) == 0 :
+      print ( "\nThere is no current filter chain to save." )
+      return
+    words = command_line.split()
+    if len(words) != 2 :
+      print ( "\n    Usage: save NAME" )
+      return
+    filter_chain_name = words[1]
+    print ( f"filter_chain_name: {filter_chain_name}" )
+    filters.current_filter_chain['name'] = filter_chain_name
+    filters.filter_chains.append(copy.deepcopy(filters.current_filter_chain))
+    filters.current_filter_chain = new.new_filter_chain()
+  
+    print ( "\nSaved Filter Chains:" )
+    for saved_filter in filters.filter_chains :
+      print ( f"    {saved_filter['name']}" )
+    print ( " " )
+  
+  
+  
+  def do_show_filter ( self, arg ) :
+    '''Show your current filter chain'''
+    mentat = self.mentat
+    if len(filters.current_filter_chain['filters']) == 0 :
+      print ( '\nThere is no current filter chain.\n' )
+      return
+  
+    print ( f"\nCurrent Filter Chain", end='' )
+    if filters.current_filter_chain['name'] == None :
+      print ( ': (no name yet)' )
+    else :
+      print ( f": {filters.current_filter_chain['name']}" )
+    
+    count = 0
+    for f in filters.current_filter_chain['filters'] :
+      print ( f"Filter {count}: {f['name']}", end='' )
+      for arg in f['args'] :
+        print ( f" {arg} ", end='' )
+      print (' ')
+      count += 1
+    print ( ' ' )
+
+
+
+  # Abbreviation
+  def do_so ( self, arg ) :
+    self.do_source ( arg )
+
+
+
+  def do_source ( self, arg ) :
+    '''Source a file of commands'''
+    file_path = arg
+
+    if not Path(file_path).is_file() :
+      print ( f"Can't find file {file_path}" )
+      return
+
+    with open(file_path, 'r') as file:
+      for line in file:
+        self.onecmd ( line.strip() )
+
+
+
+  def do_start ( self, arg ) :
+    '''Filter out all log events that happened before this time'''
+    filters.start ( self.mentat, arg )
+
+
+
+  def do_stop ( self, arg ) :
+    '''Filter out all log events that happened after this time'''
+    filters.stop ( self.mentat, arg )
+
+  # This just shows the user the start and stop times 
+  # for the current data set.
+  def do_time_range ( self, arg ) :
+    '''Show start amd stop times for entire data set'''
+    mentat = self.mentat
+    n_events        = len(mentat['events'])
+    first_timestamp = mentat['events'][0]['timestamp']
+    last_timestamp  = mentat['events'][n_events - 1]['timestamp']
+    print ( f"  {first_timestamp} to {last_timestamp}" )
+
+
+
   def do_undo ( self, arg ) :
     '''Remove the last filter that you added to your filter chain'''
     mentat = self.mentat
@@ -212,29 +263,8 @@ class MentatCLI(cmd.Cmd):
   
 
 
-  def do_list ( self, arg ) :    
-    '''List the filtered results of your current filter chain'''
-    mentat = self.mentat
-    command_line = arg
-    filters.list_filtered_results ( mentat )
-
-
-
-  def do_start ( self, arg ) :
-    '''Filter out all log events that happened before this time'''
-    filters.start ( self.mentat, arg )
-
-
-
-  def do_stop ( self, arg ) :
-    '''Filter out all log events that happened after this time'''
-    filters.stop ( self.mentat, arg )
-
-
-
-  def do_grep ( self, arg ) :
-    '''Filter log events for the given keyword'''
-    filters.grep ( self.mentat, arg )
+  def do_wait ( self, arg ) :
+    input ( )
 
 
 
